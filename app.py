fdcf92e22fd1b8e55a9556553c30007f21653b72
Cédric Schippers <cecedabro@gmail.com>
2025-08-02 08:31:56 +0200

    Quick update

--- app.py
+++ app.py
@@ -2,10 +2,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
+#Temporary mock database
+weather_data = []
+
 @app.route("/")
 def hello():
     return "Hello, Garden! and is this working?"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/api/weather", methods=["POST"])
+def receive_weather():
+    data = request.get_json()
+    weather_data.append(data)
+    return jsonify({"message": "Data recieved"}), 201
 
+@app.route("/api/weather/latest", methods=["GET"])
+def latest_weather():
+    if weather_data:
+        return jsonify(weather_data[-1])
+    else:
+        return jsonify({"message": "No data yet"}), 200
+    
+if __name__ == "__main__":
+    app.return(debug=True)
---
+++ fake_weather.py
@@ -0,0 +1,27 @@
+import requests
+import random
+import time
+
+API_URL = "https://garden-backend-bhti.onrender.com/api/weather"
+
+#Random data generator
+while True:
+    data = {
+        "temperature": round(random.uniform(15, 35) 2),
+        "humidity": round(random.uniform(30, 80), 2),
+        "wind_speed": round(random.uniform(0, 10), 2)
+    }
+
+    try:
+        response = requests.post(API_URL, json=data)
+        if response.status_code == 201:
+            print("Data sent", data)
+        else:
+            print("Error", response.status_code, response.text)
+    except Exception as e:
+        print("Failed to send data", e)
+    
+    time.sleep(10)
+
+
+    
